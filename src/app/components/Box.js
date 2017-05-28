cbafa6ebe1ee78172a86e5995d4d1fbf9042e271
Katy Morris <katymcconica@gmail.com>
2017-05-27 21:09:09 -0700

    added click event for card flip

--- src/app/components/Box.js
+++ src/app/components/Box.js
@@ -2,9 +2,16 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export class Box extends React.Component {
+
+	onBoxClick() {
+		console.log(this.div)
+		var cardBox = this.div.querySelector('.card-box')
+		cardBox.classList.add("card-flip")
+	}
+
 	render() {
 		return (
-			<div className="card-container">
+			<div className="card-container" ref={(div) => { this.div = div; }} onClick={this.onBoxClick.bind(this)}>
 				<div className="card-box">
 					<div className="card-front"></div>
 					<div className="card-back"></div>
