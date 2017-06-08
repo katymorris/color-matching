49cd494db9dbd225dab941023e225f6142c42ce6
Katy Morris <katymcconica@gmail.com>
2017-06-07 23:42:18 -0700

    added logic to check for match

--- src/app/components/Box.js
+++ src/app/components/Box.js
@@ -9,11 +9,33 @@ export class Box extends React.Component {
 		this.flipped = false
 	}
 
+	runMatchCheck(element1, element2) {
+		var box1Color = element1.querySelector(".card-back").style.backgroundColor
+		var box2Color = element2.querySelector(".card-back").style.backgroundColor
+		if (box1Color == box2Color) {
+			element1.classList.add('matched');
+			element2.classList.add('matched');
+			element1.classList.remove('flipped');
+			element2.classList.remove('flipped');
+		} else {
+			element1.classList.remove('flipped');
+			element2.classList.remove('flipped');
+			element1.classList.remove('card-flip');
+			element2.classList.remove('card-flip');
+		}
+	}
+
 	onBoxClick() {
 		var cardBox = this.div.querySelector('.card-box');
 		cardBox.classList.add("card-flip");
+		cardBox.classList.add("flipped")
 		this.flipped = true
 		
+		if (document.getElementsByClassName("flipped").length == 2) {
+			var element1 = document.getElementsByClassName("flipped")[0];
+			var element2 = document.getElementsByClassName("flipped")[1];
+			this.runMatchCheck(element1, element2)
+		}
 		
 	}
 	render() {
